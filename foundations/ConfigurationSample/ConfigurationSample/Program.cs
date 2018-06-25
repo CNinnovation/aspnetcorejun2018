726c88ea73907cad3b322745af42e34be74b8e0b
Christian Nagel <christian@christiannagel.com>
2018-06-25 15:18:46 +0200

    configuration

---
+++ foundations/ConfigurationSample/ConfigurationSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ConfigurationSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.staging.json")
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+            string val1 = config["MySetting1"];
+            Console.WriteLine(val1);
+
+            var conn1 = config.GetSection("ConnectionStrings")["Connection1"];
+            Console.WriteLine(conn1);
+
+            var conn2 = config.GetConnectionString("Connection1");
+            Console.WriteLine(conn2);
+        }
+    }
+}
