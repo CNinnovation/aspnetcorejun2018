4e6b2453d805d20dcb761439c7303526624e7dab
Christian Nagel <christian@christiannagel.com>
2018-06-28 10:50:39 +0200

    razor pages

---
+++ razorpages/RazorPagesSample/RazorPagesSample/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesSample.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        [StringLength(50)]
+        public string Title { get; set; }
+        [StringLength(20)]
+        public string Publisher { get; set; }
+    }
+}
