f2a7b6c781e3352069a9a8d6047c10f100cd6cd5
Christian Nagel <christian@christiannagel.com>
2018-06-28 10:50:29 +0200

    more mvc samples

---
+++ mvc/MyFirstMVCApp/MyFirstMVCApp/Controllers/HtmlHelpersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstMVCApp.Models;
+
+namespace MyFirstMVCApp.Controllers
+{
+    public class HtmlHelpersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var b = new Book { BookId = 1, Title = "Professional C#", Publisher = "Wrox" };
+            return View(b);
+        }
+    }
+}
