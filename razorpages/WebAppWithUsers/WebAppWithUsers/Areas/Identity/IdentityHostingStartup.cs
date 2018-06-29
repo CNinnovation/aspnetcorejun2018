2ccbaa97cfe603b190ce86a8717338c15068df48
Christian Nagel <christian@christiannagel.com>
2018-06-29 11:03:50 +0200

    Razor Pages

---
+++ razorpages/WebAppWithUsers/WebAppWithUsers/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebAppWithUsers.Data;
+
+[assembly: HostingStartup(typeof(WebAppWithUsers.Areas.Identity.IdentityHostingStartup))]
+namespace WebAppWithUsers.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
